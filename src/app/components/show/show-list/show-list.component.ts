0309ccd81bde5e62c789dbb96979af10a5095981
Amanda <esmeraldo@unifor.br>
2019-01-31 16:54:55 -0300

    use of lodash; unfortunetely had to create 2 arrays cause of fucked up structure

--- src/app/components/show/show-list/show-list.component.ts
+++ src/app/components/show/show-list/show-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShowsService } from '../../show/services/shows.service';
 import { episodes } from '../models/episodes.model';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-show-list',
@@ -11,20 +12,24 @@ export class ShowListComponent implements OnInit {
 
   constructor(private showService: ShowsService) { }
 
-  currentEpisodes = [<episodes>{}];
+  currentEpisodes1: episodes[] = [];
+  currentEpisodes2: episodes[] = [];
+  seasons: any[] = [];
 
   ngOnInit() {
     this.getShows();
   }
-  whatName($e) {
-    console.log($e);
-  }
+
   getShows() {
     this.showService.getAll().subscribe(data => {
       data.forEach(el => {
-        if (el !== null)
-          this.currentEpisodes.push(el);
-        console.log(el);
+        if (el !== null && el.SeasonNumber == 1) {
+          this.currentEpisodes1.push(el);
+        }
+        if (el !== null && el.SeasonNumber == 2) {
+          this.currentEpisodes2.push(el)
+        }
+        this.seasons = _.uniqWith(this.seasons, _.isEqual)
       });
     });
 
